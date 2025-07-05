497df5327f07395c0d68ef41eb5e54c716c4c603
Emam Mahadi_3 <globalfashionstyles822@gmail.com>
2025-07-05 07:42:26 +0000

    I’ve created a React-based eCommerce website where I’m using React.lazy

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="fixed inset-0 z-[200] shimmer-bg">
+      {/* This div creates the simple, elegant loading screen. */}
+    </div>
+  );
+}
