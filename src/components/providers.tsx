25d352082de81785931cea08208df4ceac663dda
Emam Mahadi_3 <globalfashionstyles822@gmail.com>
2025-07-06 12:34:57 +0000

    The `npm install` command failed in my project. Analyze the following er

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -6,20 +6,23 @@ import { WishlistProvider } from "@/context/wishlist-context";
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "next-themes";
 import { ProductProvider } from "@/context/product-context";
+import LenisProvider from "./lenis-provider";
 
 export function AppProviders({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-      <AuthProvider>
-        <ProductProvider>
-          <CartProvider>
-            <WishlistProvider>
-              {children}
-              <Toaster />
-            </WishlistProvider>
-          </CartProvider>
-        </ProductProvider>
-      </AuthProvider>
-    </ThemeProvider>
+    <LenisProvider>
+      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+        <AuthProvider>
+          <ProductProvider>
+            <CartProvider>
+              <WishlistProvider>
+                {children}
+                <Toaster />
+              </WishlistProvider>
+            </CartProvider>
+          </ProductProvider>
+        </AuthProvider>
+      </ThemeProvider>
+    </LenisProvider>
   );
 }
