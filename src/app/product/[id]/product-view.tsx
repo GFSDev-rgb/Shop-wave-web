0328fc61900e8a60a0f0b71a3093f973d1fb60b6
Emam Mahadi_3 <globalfashionstyles822@gmail.com>
2025-08-21 04:21:55 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/app/product/[id]/product-view.tsx
+++ src/app/product/[id]/product-view.tsx
@@ -80,7 +80,7 @@ export default function ProductView({ initialProduct }: { initialProduct: Produc
   }
 
   return (
-    <div className="container mx-auto max-w-7xl px-4 py-8 lg:py-12">
+    <div className="container mx-auto px-4 py-8 lg:py-12">
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 xl:gap-12">
         
         {/* Main Product Content */}
